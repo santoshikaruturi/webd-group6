ce00d652c8cd84431500bde26a9747e91b039a52
kirti0808 <gupta.kirti0808@gmail.com>
2019-03-31 17:16:29 +0530

    Added delete feature in admin_accept_delete_script.php

--- admin_accept_delete_script.php
+++ admin_accept_delete_script.php
@@ -7,19 +7,39 @@
 	
 <?php 
 	session_start();
-	if(isset($_SESSION['username'])){
-		$id = $_GET['id'];
-		$query="SELECT * FROM student_details WHERE id='$id' ";
-		echo $query;
-		$db_query = mysqli_query($connection, $query);
-		$res = mysqli_fetch_all($db_query);
-		if(empty($_POST)==False){
-			$query = "UPDATE student_details SET status='1' WHERE id='$id'";
-			mysqli_query($connection, $query);
-			header("Location: admin_accept_delete.php");
+	if(isset($_SESSION['username']))
+	{
+		if(isset($_POST['Accept']))
+		{
+			$id = $_GET['id'];
+			$query="SELECT * FROM student_details WHERE id='$id' ";
+			echo $query;
+			$db_query = mysqli_query($connection, $query);
+			$res = mysqli_fetch_all($db_query);
+			if(empty($_POST)==False){
+				$query = "UPDATE student_details SET status='1' WHERE id='$id'";
+				mysqli_query($connection, $query);
+				header("Location: admin_accept_delete.php");
+			}
+			else{
+				header("Location: admin_home.php");
+			}
 		}
-		else{
-			header("Location: admin_home.php");
+		else if(isset($_POST['Delete']))
+		{
+			$id = $_GET['id'];
+			$query="SELECT * FROM student_details WHERE id='$id' ";
+			echo $query;
+			$db_query = mysqli_query($connection, $query);
+			$res = mysqli_fetch_all($db_query);
+			if(empty($_POST)==False){
+				$query = "DELETE FROM student_details WHERE id='$id'";
+				mysqli_query($connection, $query);
+				header("Location: admin_accept_delete.php");
+			}
+			else{
+				header("Location: admin_home.php");
+			}
 		}
 	}
 	else{
